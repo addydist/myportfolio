980eaf3a3d86e4da6572c68d2623e03234b6c84e
Aditya pal <90037838+addydist@users.noreply.github.com>
2024-10-07 16:31:34 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -30,7 +30,7 @@ function Home2() {
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Bank end Technologies and Products </b> and
+                <b className="purple">Full stack Technologies and Products </b> and
                 also in areas related to{" "}
                 <b className="purple">
                   Blockchain and Web3
