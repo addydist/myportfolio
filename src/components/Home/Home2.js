fc930f78e584bb1977a54ef0d9dfcb47590a002a
Aditya pal <90037838+addydist@users.noreply.github.com>
2024-10-07 16:39:51 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -24,7 +24,7 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++ & Nodejs. </b>
+                <b className="purple"> Nodejs,Reactjs and C++ </b>
               </i>
               <br />
               <br />
